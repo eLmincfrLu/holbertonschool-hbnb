9b762990beaa3bee913cd981a8d110b7b9cf7a8b
eLmincfrLu <elmincfrlu@gmail.com>
2026-05-03 17:06:20 +0400

    pyhton

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restx import Api
+from app.api.v1.users import api as users_ns
 
 def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
     
-    # Placeholder: Bura sonra User, Place və s. əlavə olunacaq
+    # Namespace-i qeydiyyatdan keçiririk
+    api.add_namespace(users_ns, path='/api/v1/users')
     return app
--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,11 +1,25 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.user import User
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
 
-# Bu obyekti sonra API-larda istifadə edəcəyik
+    def create_user(self, user_data):
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return next((u for u in self.user_repo.get_all() if u.email == email), None)
+
+    def get_all_users(self):
+        return self.user_repo.get_all()
+
+    def update_user(self, user_id, user_data):
+        return self.user_repo.update(user_id, user_data)
+
 facade = HBnBFacade()
