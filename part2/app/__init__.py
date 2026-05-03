ba14791ebc2c14be53856ed3c90cbc136ae31b56
eLmincfrLu <elmincfrlu@gmail.com>
2026-05-03 17:18:20 +0400

    pyhton

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns # <-- Əlavə et
 
 def create_app():
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/')
     
-    # Namespace-i qeydiyyatdan keçiririk
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities') # <-- Əlavə et
+    
     return app
--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,25 +1,17 @@
-from app.persistence.repository import InMemoryRepository
-from app.models.user import User
+# ... əvvəlki user metodlarının altından davam et ...
 
-class HBnBFacade:
-    def __init__(self):
-        self.user_repo = InMemoryRepository()
+    def create_amenity(self, amenity_data):
+        from app.models.amenity import Amenity
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
 
-    def create_user(self, user_data):
-        user = User(**user_data)
-        self.user_repo.add(user)
-        return user
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
 
-    def get_user(self, user_id):
-        return self.user_repo.get(user_id)
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
 
-    def get_user_by_email(self, email):
-        return next((u for u in self.user_repo.get_all() if u.email == email), None)
-
-    def get_all_users(self):
-        return self.user_repo.get_all()
-
-    def update_user(self, user_id, user_data):
-        return self.user_repo.update(user_id, user_data)
-
-facade = HBnBFacade()
+    def update_amenity(self, amenity_id, amenity_data):
+        # Repository-dəki update metodunu çağırırıq
+        return self.amenity_repo.update(amenity_id, amenity_data)
