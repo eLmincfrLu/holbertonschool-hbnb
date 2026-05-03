7ff150db0f5f371e03ab68303123d56c46babbff
eLmincfrLu <elmincfrlu@gmail.com>
2026-05-03 17:19:22 +0400

    pyhton

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,5 +1,30 @@
-# ... əvvəlki user metodlarının altından davam et ...
+from app.persistence.repository import InMemoryRepository
+from app.models.user import User
 
+class HBnBFacade:
+    def __init__(self):
+        self.user_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
+
+    # User metodları
+    def create_user(self, user_data):
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return next((u for u in self.user_repo.get_all() if u.email == email), None)
+
+    def get_all_users(self):
+        return self.user_repo.get_all()
+
+    def update_user(self, user_id, user_data):
+        return self.user_repo.update(user_id, user_data)
+
+    # Amenity metodları
     def create_amenity(self, amenity_data):
         from app.models.amenity import Amenity
         amenity = Amenity(**amenity_data)
@@ -13,5 +38,6 @@
         return self.amenity_repo.get_all()
 
     def update_amenity(self, amenity_id, amenity_data):
-        # Repository-dəki update metodunu çağırırıq
         return self.amenity_repo.update(amenity_id, amenity_data)
+
+facade = HBnBFacade()
